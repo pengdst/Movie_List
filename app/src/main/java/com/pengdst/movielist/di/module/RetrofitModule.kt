d4241086b4f114e5503e5302f463aee19cf037ac
pengdst <penxkuh@gmail.com>
2020-03-31 22:01:34 +0700

    Edit Retrofit Module with DI Style

--- app/src/main/java/com/pengdst/movielist/di/module/RetrofitModule.kt
+++ app/src/main/java/com/pengdst/movielist/di/module/RetrofitModule.kt
@@ -1,16 +1,23 @@
 package com.pengdst.movielist.di.module
 
 import com.pengdst.movielist.BuildConfig
+import dagger.Module
+import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-object RetrofitModule {
-    fun connect():Retrofit {
+@Module
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun connect(): Retrofit {
         return Retrofit.Builder().apply {
                 baseUrl(BuildConfig.BASE_URL)
-                client(httpClient())
+                client(client)
                 addConverterFactory(GsonConverterFactory.create())
             }.build();
 //        return Retrofit.Builder()
@@ -19,14 +26,18 @@ object RetrofitModule {
 //            .build();
     }
 
-    private fun httpClient():OkHttpClient{
+    @Provides
+    @Singleton
+    fun httpClient():OkHttpClient{
         return OkHttpClient.Builder().apply {
             retryOnConnectionFailure(true)
-            addInterceptor(httpInterceptor())
+            addInterceptor(interceptor)
         }.build()
     }
 
-    private fun httpInterceptor():HttpLoggingInterceptor{
+    @Provides
+    @Singleton
+    fun httpInterceptor():HttpLoggingInterceptor{
         return HttpLoggingInterceptor().apply {
             level = when (BuildConfig.DEBUG){
                 true -> HttpLoggingInterceptor.Level.BODY
