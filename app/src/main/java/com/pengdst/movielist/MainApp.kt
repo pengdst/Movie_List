9facde07fabca2d2662f49a6fb02e8fadec00c57
pengdst <penxkuh@gmail.com>
2020-03-31 22:31:14 +0700

    Update MainApp

--- app/src/main/java/com/pengdst/movielist/MainApp.kt
+++ app/src/main/java/com/pengdst/movielist/MainApp.kt
@@ -1,5 +1,12 @@
 package com.pengdst.movielist
 
+import com.pengdst.movielist.di.component.ApplicationComponent
+import dagger.android.AndroidInjection.inject
+import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
-class MainApp : DaggerApplication()
+class MainApp : DaggerApplication(){
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return ApplicationComponent.create().apply{ inject(this@MainApp) }
+    }
+}
--- app/src/main/java/com/pengdst/movielist/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/pengdst/movielist/di/component/ApplicationComponent.kt
@@ -1,2 +1,18 @@
 package com.pengdst.movielist.di.component
 
+import com.pengdst.movielist.MainApp
+import com.pengdst.movielist.di.builder.ActivityBuilder
+import com.pengdst.movielist.di.module.RetrofitModule
+import dagger.Component
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    RetrofitModule::class,
+    ActivityBuilder::class
+])
+interface ApplicationComponent : AndroidInjector<MainApp>
