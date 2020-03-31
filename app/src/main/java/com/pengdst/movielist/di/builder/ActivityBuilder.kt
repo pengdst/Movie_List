f33ad0a00c0f071f4354b444dc6fe17a88271923
pengdst <penxkuh@gmail.com>
2020-03-31 22:17:54 +0700

    Update ActivityBuilder in package builder

--- app/src/main/java/com/pengdst/movielist/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/pengdst/movielist/di/builder/ActivityBuilder.kt
@@ -1,2 +1,15 @@
 package com.pengdst.movielist.di.builder
 
+import com.pengdst.movielist.di.module.MainModule
+import com.pengdst.movielist.di.scope.Presentation
+import com.pengdst.movielist.presentation.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuilder {
+
+    @Presentation
+    @ContributesAndroidInjector(modules = [MainModule::class])
+    abstract fun contributeMainActivity(): MainActivity
+}
