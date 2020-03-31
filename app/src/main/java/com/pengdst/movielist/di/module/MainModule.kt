0347b435518e8bc9a157b15924ebd673c2d43db4
pengdst <penxkuh@gmail.com>
2020-03-31 22:12:02 +0700

    Updaye MainModule in package module

--- app/src/main/java/com/pengdst/movielist/di/module/MainModule.kt
+++ app/src/main/java/com/pengdst/movielist/di/module/MainModule.kt
@@ -1,4 +1,32 @@
 package com.pengdst.movielist.di.module
 
+import com.pengdst.movielist.datas.routes.MovieRoute
+import com.pengdst.movielist.presentation.MainActivity
+import com.pengdst.movielist.presentation.MainPresenter
+import com.pengdst.movielist.presentation.MainView
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+
+@Module
 abstract class MainModule {
+
+    @Module
+    companion object{
+        @JvmStatic
+        @Provides
+        fun provideMovieRoute(retrofit: Retrofit) =
+            retrofit.create(MovieRoute::class.java)
+
+        @JvmStatic
+        @Provides
+        fun provideMainPresenter(
+            mainView: MainView,
+            movieRoute: MovieRoute
+        ):MainPresenter = MainPresenter(mainView, movieRoute)
+    }
+
+    @Binds
+    abstract fun bindMainView(activity: MainActivity): MainView
 }
